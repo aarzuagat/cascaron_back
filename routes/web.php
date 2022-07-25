0b9e047c523bd344f9baf2b5eaa029cc1fef6dda
Arzuaga <aarzuagat@gmail.com>
2022-07-25 08:58:05 -0400

    Arregla el error al Imprimir el stock de etiquetas

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 //Route::get('/', function () {
 //    return view('welcome');
 //});
+Route::get('/storage{any}')->where('any', '.*');
 Route::get('{any}', function () {
     return view('welcome');
-})->where('any','.*');
+})->where('any', '^(?!api).*$');
