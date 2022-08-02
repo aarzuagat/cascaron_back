0b6f5b6ccb6e1575a97104db7afdd80f3e0bcc11
Arzuaga <aarzuagat@gmail.com>
2022-08-02 19:03:00 -0400

    desplegando

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,6 @@
 <div id="q-app" v-cloak>
 </div>
 <script src=/js/vendor.9f6dbb2b.js></script>
-<script src=/js/app.6fe12a01.js></script>
+<script src=/js/app.84a4cec5.js></script>
 </body>
 </html>
